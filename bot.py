7b94fa0a4a36c11be6498929182e7317a1950dd0
supermanbot12 <98998991+supermanbot12@users.noreply.github.com>
2022-06-08 12:17:29 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import pyromod.listen
 from pyrogram import Client
 import sys
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL1, FORCE_SUB_CHANNEL2, CHANNEL_ID
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL1, FORCE_SUB_CHANNEL2, FORCE_SUB_CHANNEL3, CHANNEL_ID
 
 class Bot(Client):
     def __init__(self):
@@ -32,7 +32,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL1 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL1}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/OuraaSupport for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/supermansupportt for support")
                 sys.exit()
         if FORCE_SUB_CHANNEL2:
             try:
@@ -42,7 +42,17 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
-                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/OuraaSupport for support")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/supermansupportt for support")
+                sys.exit()
+        if FORCE_SUB_CHANNEL3:
+            try:
+                link = await self.export_chat_invite_link(FORCE_SUB_CHANNEL3)
+                self.invitelink3 = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
+                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL3 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL3}")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/supermansupportt for support")
                 sys.exit()
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
@@ -52,11 +62,11 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/OuraaSupport for support")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/supermansupportt for support")
             sys.exit()
 
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by Lord Ouraa\nhttps://t.me/Owaitingforyou")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by superman\nhttps://t.me/gkushskap")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
